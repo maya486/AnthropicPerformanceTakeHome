e366ea84638e2ed26d61a4bee5b0d3c490038d5f
Maya Rebholz <maya.rebholz@gmail.com>
2026-07-17 16:43:36 -0400

    112918: Got rid of select control flow in idx update and rearranged loops such that only read and write mem_indices and mem_values once total

--- perf_takehome.py
+++ perf_takehome.py
@@ -130,43 +130,41 @@ class KernelBuilder:
         tmp_val = self.alloc_scratch("tmp_val")
         tmp_node_val = self.alloc_scratch("tmp_node_val")
         tmp_addr = self.alloc_scratch("tmp_addr")
+        tmp_val_parity = self.alloc_scratch("tmp_val_parity")
 
-        for round in range(rounds):
-            for i in range(batch_size):
-                i_const = self.scratch_const(i)
-                # idx = mem[inp_indices_p + i]
-                body.append(("alu", ("+", tmp_addr, self.scratch["inp_indices_p"], i_const)))
-                body.append(("load", ("load", tmp_idx, tmp_addr)))
-                body.append(("debug", ("compare", tmp_idx, (round, i, "idx"))))
-                # val = mem[inp_values_p + i]
-                body.append(("alu", ("+", tmp_addr, self.scratch["inp_values_p"], i_const)))
-                body.append(("load", ("load", tmp_val, tmp_addr)))
-                body.append(("debug", ("compare", tmp_val, (round, i, "val"))))
+        for walker_idx in range(batch_size):
+            walker = self.scratch_const(walker_idx)
+
+            # idx = mem[inp_indices_p + i]
+            body.append(("alu", ("+", tmp_addr, self.scratch["inp_indices_p"], walker)))
+            body.append(("load", ("load", tmp_idx, tmp_addr)))
+            # val = mem[inp_values_p + i]
+            body.append(("alu", ("+", tmp_addr, self.scratch["inp_values_p"], walker)))
+            body.append(("load", ("load", tmp_val, tmp_addr)))
+
+            for round in range(rounds):
                 # node_val = mem[forest_values_p + idx]
                 body.append(("alu", ("+", tmp_addr, self.scratch["forest_values_p"], tmp_idx)))
                 body.append(("load", ("load", tmp_node_val, tmp_addr)))
-                body.append(("debug", ("compare", tmp_node_val, (round, i, "node_val"))))
                 # val = myhash(val ^ node_val)
                 body.append(("alu", ("^", tmp_val, tmp_val, tmp_node_val)))
-                body.extend(self.build_hash(tmp_val, tmp1, tmp2, round, i))
-                body.append(("debug", ("compare", tmp_val, (round, i, "hashed_val"))))
-                # idx = 2*idx + (1 if val % 2 == 0 else 2)
-                body.append(("alu", ("%", tmp1, tmp_val, two_const)))
-                body.append(("alu", ("==", tmp1, tmp1, zero_const)))
-                body.append(("flow", ("select", tmp3, tmp1, one_const, two_const)))
+                body.extend(self.build_hash(tmp_val, tmp1, tmp2, round, walker_idx))
+
+                # idx = 2*idx + 1 + val&1
+                body.append(("alu", ("&", tmp_val_parity, tmp_val, one_const)));
                 body.append(("alu", ("*", tmp_idx, tmp_idx, two_const)))
-                body.append(("alu", ("+", tmp_idx, tmp_idx, tmp3)))
-                body.append(("debug", ("compare", tmp_idx, (round, i, "next_idx"))))
+                body.append(("alu", ("+", tmp_idx, tmp_idx, one_const)))
+                body.append(("alu", ("+", tmp_idx, tmp_idx, tmp_val_parity)))
+
                 # idx = 0 if idx >= n_nodes else idx
                 body.append(("alu", ("<", tmp1, tmp_idx, self.scratch["n_nodes"])))
                 body.append(("flow", ("select", tmp_idx, tmp1, tmp_idx, zero_const)))
-                body.append(("debug", ("compare", tmp_idx, (round, i, "wrapped_idx"))))
-                # mem[inp_indices_p + i] = idx
-                body.append(("alu", ("+", tmp_addr, self.scratch["inp_indices_p"], i_const)))
-                body.append(("store", ("store", tmp_addr, tmp_idx)))
-                # mem[inp_values_p + i] = val
-                body.append(("alu", ("+", tmp_addr, self.scratch["inp_values_p"], i_const)))
-                body.append(("store", ("store", tmp_addr, tmp_val)))
+            # mem[inp_indices_p + i] = idx
+            body.append(("alu", ("+", tmp_addr, self.scratch["inp_indices_p"], walker)))
+            body.append(("store", ("store", tmp_addr, tmp_idx)))
+            # mem[inp_values_p + i] = val
+            body.append(("alu", ("+", tmp_addr, self.scratch["inp_values_p"], walker)))
+            body.append(("store", ("store", tmp_addr, tmp_val)))
 
         body_instrs = self.build(body)
         self.instrs.extend(body_instrs)
