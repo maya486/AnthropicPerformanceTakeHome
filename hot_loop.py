cb1e3ffe6c9872fb20636862188e386f1f7ee014
Maya Rebholz <maya.rebholz@gmail.com>
2026-08-09 12:33:10 -0400

    2258: cleaned code more, made walker_group.py, now main method pretty simple

--- hot_loop.py
+++ hot_loop.py
@@ -3,7 +3,7 @@ from load_instrs import gen_load_instrs_generic
 from hash_and_update_instrs import gen_hash_and_update_instrs_generic
 from packer import pack, merge_independent_instr_streams
 
-def gen_hot_loop_generic(round, phase, forest_height, forest_values, group_size, const_operands, tmps, consts):
+def gen_hot_loop_generic(round, forest_height, forest_values, group_size, const_operands, tmps, consts):
     hash_and_update_instrs = gen_hash_and_update_instrs_generic((round-1)//2, (round-1)%2, forest_height, group_size, tmps, consts)
     index_instrs = gen_index_calculation_instrs_generic(round%2, group_size, consts, tmps)
     packed_valu_instrs = pack(index_instrs + hash_and_update_instrs, const_operands)
--- perf_takehome.py
+++ perf_takehome.py
@@ -41,6 +41,8 @@ from index_calculation_instrs import gen_index_calculation_instrs_generic
 from load_instrs import gen_load_instrs_generic
 from hash_and_update_instrs import gen_hash_and_update_instrs_generic
 
+from walker_group import gen_read_in_walker_group_instrs, gen_write_out_walker_group_instrs
+
 from packer import pack
 from setup import setup_scratch
 
@@ -87,13 +89,6 @@ class KernelBuilder:
 
         all_instrs = []
 
-        # setup = []
-        walker_group_prologue = []
-        hot_loop = []
-        walker_group_epilogue = []
-
-
-
         # Pause instructions are matched up with yield statements in the reference
         # kernel to let you debug at intermediate steps. The testing harness in this
         # file requires these match up to the reference kernel's yields, but the
@@ -110,63 +105,38 @@ class KernelBuilder:
         all_instrs.extend(pack(setup, const_operands))
 
 
+        # fully process groups of walkers at a time
         for walker_idx in range(0, num_walkers, 2*group_size):
 
             walker_idx_idx = (int)(walker_idx/(2*group_size))
 
-            # load index for every walker in group_size
-            # idx = mem[inp_indices_p + i]
-            # val = mem[inp_values_p + i]
-            for phase in range(num_phases):
-                for i in range(group_size//VLEN):
-                    walker_group_prologue.append(("alu", ("+", tmps["addrs"][phase][i], consts["inp_indices_p"], consts["walker_idxs"][walker_idx_idx][phase][i])))
-
-            for phase in range(num_phases):
-                for i in range(group_size//VLEN):
-                    walker_group_prologue.append(("load", ("vload", tmps["idxs"][phase][i], tmps["addrs"][phase][i])))
-
-            for phase in range(num_phases):
-                for i in range(group_size//VLEN):
-                    walker_group_prologue.append(("alu", ("+", tmps["addrs"][phase][i], consts["inp_values_p"], consts["walker_idxs"][walker_idx_idx][phase][i])))
-
-            for phase in range(num_phases):
-                for i in range(group_size//VLEN):
-                    walker_group_prologue.append(("load", ("vload", tmps["vals"][phase][i], tmps["addrs"][phase][i])))
-
-            walker_group_prologue.extend(gen_index_calculation_instrs_generic(0, group_size, consts, tmps))
-            walker_group_prologue.extend(gen_load_instrs_generic(0, group_size, tmps))
-
-            all_instrs.extend(pack(walker_group_prologue, const_operands))
             walker_group_prologue = []
 
+            # read in all walker group vals and idxs
+            walker_group_prologue.extend(gen_read_in_walker_group_instrs(walker_idx_idx, num_phases, group_size, tmps, consts))
+
+            # start pipeline
+            walker_group_prologue.extend(gen_index_calculation_instrs_generic(0, group_size, consts, tmps))
+            walker_group_prologue.extend(gen_load_instrs_generic(0, group_size, tmps))
+            all_instrs.extend(pack(walker_group_prologue, const_operands))
+
+            # main body of pipeline
             for round in range(1, 2*rounds):
 
-                hot_loop = gen_hot_loop_generic(round, phase, forest_height, consts["forest_values"], group_size, const_operands, tmps, consts)
+                hot_loop = gen_hot_loop_generic(round, forest_height, consts["forest_values"], group_size, const_operands, tmps, consts)
 
                 all_instrs.extend(hot_loop)
 
+            walker_group_epilogue = []
+
+            # finish pipeline
             walker_group_epilogue.extend(gen_hash_and_update_instrs_generic(2*rounds-1, (rounds-1)%2, forest_height, group_size, tmps, consts))
 
-            # mem[inp_indices_p + i] = idx
-            # mem[inp_values_p + i] = val
-            for phase in range(num_phases):
-                for i in range(group_size//VLEN):
-                    walker_group_epilogue.append(("alu", ("+", tmps["addrs"][phase][i], consts["inp_indices_p"], consts["walker_idxs"][walker_idx_idx][phase][i])))
 
-            for phase in range(num_phases):
-                for i in range(group_size//VLEN):
-                    walker_group_epilogue.append(("store", ("vstore", tmps["addrs"][phase][i], tmps["idxs"][phase][i])))
-
-            for phase in range(num_phases):
-                for i in range(group_size//VLEN):
-                    walker_group_epilogue.append(("alu", ("+", tmps["addrs"][phase][i], consts["inp_values_p"], consts["walker_idxs"][walker_idx_idx][phase][i])))
-
-            for phase in range(num_phases):
-                for i in range(group_size//VLEN):
-                    walker_group_epilogue.append(("store", ("vstore", tmps["addrs"][phase][i], tmps["vals"][phase][i])))
+            # write out all walker group vals and idxs
+            walker_group_epilogue.extend(gen_write_out_walker_group_instrs(walker_idx_idx, num_phases, group_size, tmps, consts))
 
             all_instrs.extend(pack(walker_group_epilogue, const_operands))
-            walker_group_epilogue = []
 
 
         # Required to match with the yield in reference_kernel2
